5975c9634eec270caceb4d62c56946de2b66c6ed
sudip lamsal <lamsal.sudip99@gmail.com>
2022-09-02 17:35:30 +0900

    website added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import About from './About'
+import { Fotter } from './Fotter'
+import { MainBanner } from './MainBanner'
+import NavBar from './NavBar'
+import "./style.css"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div><NavBar/>
+         <MainBanner/>
+         <About/>
+         <Fotter/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/Fotter.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Fotter = () => {
+  return (
+    <footer id="footer">
+        <div className="container-fluid">
+            <div className="social-icons mt-4">
+                <a href="https://www.facebook.com/sudip.lamsal.9/" target="_blank"><i className="fab fa-facebook"></i></a>
+                <a href="https://instagram.com/sudiplamsal?igshid=YmMyMTA2M2Y=" target="_blank"><i className="fab fa-instagram"></i></a>
+            </div>
+        </div>
+    </footer>
+
+  )
+}
